aed5e52f9af72c75b3bac5850e9eadbd51e5021a
ahmxdz <ahmedali6342@gmail.com>
2022-12-07 13:50:35 -0500

    mongoose added

--- personal-portfolio-ahmed/server/config/database.js
+++ personal-portfolio-ahmed/server/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/portfolio', {
+mongoose.connect('mongodb://localhost:27017/portfolio', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
